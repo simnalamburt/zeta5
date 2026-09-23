b694dbf52a101e7e1300c79e8a20841fe0477bef
Jihyeon Kim <simnalamburt@gmail.com>
2026-09-23 18:14:03 +0900

    License under (APACHE-2.0 OR MIT)

--- Zeta5/Main.lean
+++ Zeta5/Main.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Jihyeon Kim. All rights reserved.
+Released under Apache 2.0 or MIT license, at your option, as described in the file COPYRIGHT.
+Authors: Jihyeon Kim
+-/
 import Mathlib.NumberTheory.LSeries.RiemannZeta
 import Mathlib.NumberTheory.Real.Irrational
 
